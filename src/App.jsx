8dca8b8027390bc28a4b8489af9db8caa9806f1a
Prathamesh Gadekar <93116210+Pr0-C0der@users.noreply.github.com>
2024-10-05 13:14:19 +0000

    Configure route to home page

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,10 @@ const App = () => {
         <div className="flex-grow bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
           <main className="content-wrapper container mx-auto px-4 py-8 pt-20">
             <Routes>
+              {/* Route for both "/" and "/portfolio/" */}
               <Route path="/" element={<Home />} />
+              <Route path="/portfolio/" element={<Home />} />
+
               <Route path="/education" element={<Education />} />
               <Route path="/experience" element={<Experience />} />
               <Route path="/projects" element={<Projects />} />
