975cdb3caa6361c5407f7d13a4c1727c7586ee65
Prathamesh Gadekar <93116210+Pr0-C0der@users.noreply.github.com>
2024-10-05 13:46:32 +0000

    comm

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio',  // Replace with your repository name
+  base: '',  // Replace with your repository name
   server: {
     host: "::",
     port: "8080",
